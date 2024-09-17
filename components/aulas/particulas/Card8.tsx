3cfca261fd168fcf895256d5a2e69c3c98683b33
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2024-09-17 20:46:43 +0000

    adds image

--- components/aulas/particulas/Card8.tsx
+++ components/aulas/particulas/Card8.tsx
@@ -1,4 +1,5 @@
 import { Card, CardTitle } from "@/components/ui/card";
+import Image from "next/image";
 
 export default function Card7() {
   return (
@@ -6,7 +7,7 @@ export default function Card7() {
       <CardTitle>Resumo</CardTitle>
 
       <div className="text-gray-700 leading-relaxed space-y-4">
-        <p>IMAGEM</p>
+        <Image src="/resumo.jpeg" width={500} height={500} alt="Resumo" />
       </div>
     </Card>
   );
